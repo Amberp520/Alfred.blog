f296de358984bcf0046e1f59042e28d782061256
Amber <emenikep520@gmail.com>
2025-01-21 06:01:56 -0800

    first commit

---
+++ js/blog.js
@@ -0,0 +1,87 @@
+export const blogs = [
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/chill guy.jpeg",
+    blogTitle: "Chill Guy meme token drops",
+    blogStory:
+      "<p class='text'>You must have heard about the fall of chill guy meme coin now hears the catch to that news<span class='dots'>....</span><span class='moreText'>Chill Guy meme coin's value has taken a hit lately. After reaching an all-time high of $0.6651 on November 27, 2024, its price dropped by 69.29% . The coin's market capitalization also fell significantly, from $488 million to around $204 million The decline is attributed to several factors, including increased competition from other meme coins and concerns over intellectual property rights violations by the team behind Chill Guy . Additionally, the coin's creator, Phillip Banks, copyrighted the artwork and announced that he would issue takedowns for unauthorized merchandise and shitcoins .It's worth noting that the Chill Guy meme coin's popularity was largely driven by its viral presence on social media platforms like TikTok, where it was used to promote relaxation and self-care . However, its decline in value has led some investors to look elsewhere, with Doge Uprising being touted as a potential successor in the meme coin space.</span></p><button class='append'>Read More</button>",
+    date: "19/12/2024" ,
+    time: "11:25pm",
+  },
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/crypto pic.jpg",
+    blogTitle: "Knowing the good project to invest in",
+    blogStory:
+      "<div class='text1'><p>Finding the right project to invest in can be quite chanllenging, due to avoiding what you call a <strong>RUG-PULL</strong> and this can be avoidable through a very good research.<span class='dots1'>...</span></p><span class='moreText1'>Firstly do a research on these 5 key points-<ul><li>Founder History: Do a good  research on the founding fathers of the projects. Check how there records have been find our if they have worked for any preveious known projects.</li></ul><ul><li> Inflow and Outflow transactions: Know how much people are buy and how much people are selling.This can be a good way to know how the project transaction stablity. </li></ul><ul><li>Know top liqdity providers: Knowing the liqudity providers is something people tend to miss when doing their research on a project once you see an indiviual holding more than 25% of crypto currency you shouldn't invest in that project cause that indiviual is likely to sell at any point in time.</li></ul><ul><li>Whitepaper: You might have come across the word whitepaper and you can't help but wonder what it means, now see a white paper as a a blueprint that gives the detail of how the structure of the building would be, so bascially it's a detailed diagram telling you what the project is about. Loads people tend to ignore a white paper but it's one of the key items that make's you truly understand what your investing in.</li></ul><ul><li> Social media & Community: Checking up on a projects social media is one of the best ways to know more about the project and there community. This helps to know how strong there community is, cause is these key points that can determine the feature of the project. But what mostly is important in the two is community you need a strong community for your project to go far. </li></ul></span></div><button class='append1'>Read More</button>",
+    date: "19/12/2024",
+    time: "11:25pm",
+  },
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/ratin.jpg",
+    blogTitle: "Getting to know what a market cap and a circulating supply is",
+    blogStory:
+      "<div class='text2'><p>Now you might have come across these two words market cap and circulating supply and in this artcile i will tell you what they mean and how they work hand in hand.<span class='dots2'>...</span></p><span class='moreText2'>First what does Market cap simply mean:<ul><li>A market cap is the total value a project's outstanding shares or circulating supply it's worth lets use bitcoin as an example bitcoin's market cap is worth $1.8 trillion now now you must wounder how is that caculated you cacylate the current price and the circulating supply (current price: $95,364* circulating supply:19,800,000=$1.8 trillion) and this example goes for all projects.</li></ul><ul><li>Circulating supply: Circulating supply is the total outstanding shares that are tradeable to investors now other than circulating supply we have total supply which is the the total supply of coins a project has in existence let's use bitcoin as an example bitcoin's total supply is 21 million while there circulating supply is 19.8 million. </li></ul></p></span></div><button class='append2'>Read More</button>",
+    date: "19/12/2024",
+    time: "11:25pm",
+  },
+
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/Wallet (2).jpg",
+    blogTitle: "What is Custodial Wallet and Non-Custodial Wallet ",
+    blogStory:
+      "<div class='text3'><p>In the Crypto space, wallets are divided in two places namely.<span class='dots3'>....</span></p><span class='moreText3'>Custodial wallet and Non-Custodial wallet:<ul><li>Custodial Wallet: A Custodial wallet is a third-party service provider that holds and manages your cryptocurrencies they also take control of your private keys and your funds examples of custodial wallets are (Binance,Coinbase)</li></ul><ul><li>Non-Custodial wallet: A Non-coustodial wallet is a wallet that does have any third party provider your the one who managing your funds and you also get access to your private keys example of Non-custodial wallets (Trust wallet,Metamask)</li></ul></span></div><button class='append3'>Read More</button>",
+    date: "19/12/2024",
+    time: "11:25pm",
+  },
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/solidity.jpg",
+    blogTitle: "What is Solidity?",
+    blogStory:
+      "<p class='text4'>Solidity is a programming language used for writting smart contract on the Ethereum blockchain.<span class='dots4'>....</span><span class='moreText4'>It's a contract-oriented language, made to  help developers to create self-excecuting contracts with the terms of the agreement written in lines of code. Solidity is influenced by lanuages like JavaScript,Python, and C++ and it sums up to bytecode that can be executed on the Ethereum Virtual Machine(EVM) but the language   out of the 3 that is closest to Soildity is JavaScript you have to have a core understanding of JavaScript before moving over to Soildity.</span></p><button class='append4'>Read More</button> ",
+    date: "19/12/2024",
+    time: "11:25pm",
+  },
+  {
+    author: "Onwusowulu ikechukwu",
+    blogImage: "./images/hippo.jpg",
+    blogTitle:
+      "Ethereum co-founder Vitalik Buterin adopts viral baby hippo Moo-Den with 88-ETH ",
+    blogStory:
+      "<div class='text5'><p>Vitalika Buterin, Ethereum's co-founder,<span class='dots5'>....</span></p><span class='moreText5'><p> Has adopted Moo Deng, a pygmy hipopotamus at the Khao Kheow Open Zoo in Thailand.</p> <p> He made a good sum of donation 88-ETH($290,000) to support the zoo and the hippo's upkeep.</p> <p> Moo Deng, known for her cuteness, became a popular meme coin on Solana's blockchain (MOODENG), which has an impressive price growth. Buterin's involvement showcases his love and zeal toward both wildlife and conservation and the growing influence of cryptocurrency in real-world initiatives.</p></span></div><button class='append5'>Read More</button>",
+    date: "27/12/2024",
+    time: "11:25pm",
+  },
+  {
+    author: "Odu, Stephen Anyaku III",
+    blogImage: "./images/step.jpg",
+    blogTitle: "BITCOIN IS A REAL STORE OF ASSET VALUE, BUT IS IT? ",
+    blogStory:
+      "<div class='text6'><p>It's been said so many times that now, it's become one of those things you know but don't know how you know it. It's like one of those unwritten rules in the gym.<span class='dots6'>...</span></p> <span class='moreText6'><p>But on the flip side, is it really? This will sound like a lot of mumbo jumbo but just walk with me.</p><ul><strong>For something to be said to be a store of asset value, it has to meet certain criteria which include:</strong> <li>✅ It derives its innate value from its utility.</li> <li>✅ It isn't affected by the rally and dump of currencies.</li><li>✅ The value of currencies are determined by how well they do against it.</li></ul> <p>This looks like a lot of grey areas and vague terms but it's not.</p><ul><strong>Let’s look at the Gold</strong> <li>✅ It has innate value in itself as a precious metal. </li> <li>✅ A dump in the dollar or pounds doesn’t imply depreciation in it’s actually value, it’ll just pump against it so it’s not affected by the dollar inflation.</li><li> ✅ It’s a reserve asset, the Great Britain Pounds gets it’s name from it’s pound worth of gold. </li></ul> <p>But unlike gold, whenever the dollar dumps, bitcoin retains its value in the dollar but its actual value is reduced because what it's tied to isn't inflation proof. </p> <p> Am I saying you shouldn't buy bitcoin? No, that'll be the demons of your father's house counselling you. I'm just saying we should use the right terms and not click baits.</p>  <p>Thank you</p> <p>PS: Hitting the Market's Curve Ball begins tomorrow so stay giggy! </p></span></div><button class='append6'>Read More</button>",
+    date: "19/24/2024",
+    time: "11:25pm",
+  },
+
+  {
+    author: "Odu, Stephen Anyaku III",
+    blogImage: "./images/Office.jpg",
+    blogTitle: "BITCOIN IS A REAL STORE OF ASSET VALUE, BUT IS IT? ",
+    blogStory:
+      "<div class='text6'><p>It's been said so many times that now, it's become one of those things you know but don't know how you know it. It's like one of those unwritten rules in the gym.<span class='dots6'>...</span></p> <span class='moreText6'><p>But on the flip side, is it really? This will sound like a lot of mumbo jumbo but just walk with me.</p><ul><strong>For something to be said to be a store of asset value, it has to meet certain criteria which include:</strong> <li>✅ It derives its innate value from its utility.</li> <li>✅ It isn't affected by the rally and dump of currencies.</li><li>✅ The value of currencies are determined by how well they do against it.</li></ul> <p>This looks like a lot of grey areas and vague terms but it's not.</p><ul><strong>Let’s look at the Gold</strong> <li>✅ It has innate value in itself as a precious metal. </li> <li>✅ A dump in the dollar or pounds doesn’t imply depreciation in it’s actually value, it’ll just pump against it so it’s not affected by the dollar inflation.</li><li> ✅ It’s a reserve asset, the Great Britain Pounds gets it’s name from it’s pound worth of gold. </li></ul> <p>But unlike gold, whenever the dollar dumps, bitcoin retains its value in the dollar but its actual value is reduced because what it's tied to isn't inflation proof. </p> <p> Am I saying you shouldn't buy bitcoin? No, that'll be the demons of your father's house counselling you. I'm just saying we should use the right terms and not click baits.</p>  <p>Thank you</p> <p>PS: Hitting the Market's Curve Ball begins tomorrow so stay giggy! </p></span></div><button class='append6'>Read More</button>",
+    date: "19/24/2024",
+    time: "11:25pm",
+  },
+
+  {
+    author: "Odu, Stephen Anyaku III",
+    blogImage: "./images/coin.jpg",
+    blogTitle: "BITCOIN IS A REAL STORE OF ASSET VALUE, BUT IS IT? ",
+    blogStory:
+      "<div class='text6'><p>It's been said so many times that now, it's become one of those things you know but don't know how you know it. It's like one of those unwritten rules in the gym.<span class='dots6'>...</span></p> <span class='moreText6'><p>But on the flip side, is it really? This will sound like a lot of mumbo jumbo but just walk with me.</p><ul><strong>For something to be said to be a store of asset value, it has to meet certain criteria which include:</strong> <li>✅ It derives its innate value from its utility.</li> <li>✅ It isn't affected by the rally and dump of currencies.</li><li>✅ The value of currencies are determined by how well they do against it.</li></ul> <p>This looks like a lot of grey areas and vague terms but it's not.</p><ul><strong>Let’s look at the Gold</strong> <li>✅ It has innate value in itself as a precious metal. </li> <li>✅ A dump in the dollar or pounds doesn’t imply depreciation in it’s actually value, it’ll just pump against it so it’s not affected by the dollar inflation.</li><li> ✅ It’s a reserve asset, the Great Britain Pounds gets it’s name from it’s pound worth of gold. </li></ul> <p>But unlike gold, whenever the dollar dumps, bitcoin retains its value in the dollar but its actual value is reduced because what it's tied to isn't inflation proof. </p> <p> Am I saying you shouldn't buy bitcoin? No, that'll be the demons of your father's house counselling you. I'm just saying we should use the right terms and not click baits.</p>  <p>Thank you</p> <p>PS: Hitting the Market's Curve Ball begins tomorrow so stay giggy! </p></span></div><button class='append6'>Read More</button>",
+    date: "19/24/2024",
+    time: "11:25pm",
+  },
+];
